b145b660c9bdacd3fdb31b5e558324374beedc94
王鹏 <306466972@qq.com>
2021-05-14 10:15:09 +0800

    add files

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="app">
+    <div id="nav">
+
+      <van-button type="info">信息按钮</van-button>
+      <van-button type="default">默认按钮</van-button>
+    </div>
+    <router-view />
+  </div>
+</template>
+
+<style lang="less">
+</style>
